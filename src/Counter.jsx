a761f218ae646e93fc75a19cc1bb5d04eed598e4
ankita_rupale <ankitarupale0308@gmail.com>
2026-06-16 16:55:03 +0530

    added limit to add and remove

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -5,18 +5,20 @@ const Counter = () => {
     const [counter, setCounter] = useState(15);
 
     const addValue = () => {
-        setCounter(counter+1)
+        // setCounter(counter+1)
+        counter < 20 ? setCounter(counter + 1) : counter;
     }
 
     const removeValue = () => {
-        setCounter(counter-1)
+        // setCounter(counter-1)
+       counter > 0 ? setCounter(counter - 1) : counter;
     }
 
     return (
         <div>
             <h1>Counter: {counter}</h1>
-            <button onClick={addValue}>Add value: {counter}</button>
-            <button onClick={removeValue}>Remove value: {counter}</button>
+            <button onClick={addValue} disabled={counter >= 20}>Add value: {counter}</button>
+            <button onClick={removeValue} disabled={counter <= 0}>Remove value: {counter}</button>
         </div>
     );
 }
